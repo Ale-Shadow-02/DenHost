f99781d11ebcc21c8b700d89a93ac9b13b3838ba
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2022-05-18 12:45:45 +0400

    В разработке форма обратной связи

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as Popup from "./modules/popup.js";
 
 flsFunctions.isWebp();
 flsFunctions.mobilMenu();
 flsFunctions.circle();
 flsFunctions.scrollTop();
+Popup.modalOpen();
 
 
 /*
